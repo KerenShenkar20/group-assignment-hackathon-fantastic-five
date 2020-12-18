8c6530acff256545547b4721e59974e1af37bf26
Yarin Mizrahi <yarinmizrahi@Yarins-MacBook-Pro.local>
2020-12-18 12:05:48 +0200

    POST route fixed

--- Routers/user.router.js
+++ Routers/user.router.js
@@ -9,7 +9,7 @@ module.exports = { userRouter };
 userRouter.get('/',userController.getUsers);
 userRouter.get('/:id',userController.getUser);
 userRouter.get('/filter',userController.filterUsers);
-userRouter.post('/:id',userController.addUser);
+userRouter.post('/',userController.addUser);
 userRouter.put('/:id',userController.updateUser);
 userRouter.delete('/:id',userController.deleteUser);
 
