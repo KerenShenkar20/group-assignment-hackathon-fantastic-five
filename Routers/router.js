8360af770894be9b0381519d03ea1b5715f86102
Yarin Mizrahi <yarinmizrahi@Yarins-MacBook-Pro.local>
2020-12-18 11:32:17 +0200

    user.router

--- Routers/router.js
+++
@@ -1,9 +0,0 @@
-const { Router } = require('express');
-const {userController} = require('../Controllers/user.ctrl');
-
-
-const userRouter = new Router();
-module.exports = { userRouter };
-
-// userRouter.get('/',userController.getUsers)
-
--- Routers/user.router.js
+++ Routers/user.router.js
@@ -1,1 +1,19 @@
-// Change the name of the file if needed
+// Change the name of the file if neededconst { Router } = require('express');
+const {userController} = require('../Controllers/user.ctrl');
+
+
+const userRouter = new Router();
+module.exports = { userRouter };
+
+userRouter.get('/',userController.getUsers);
+userRouter.get('/:id',userController.getUser);
+userRouter.get('/:id',userController.getUser);
+userRouter.get('/:id',userController.getUser);
+userRouter.put('/:id',userController.updateuser);
+
+
+
+
+
+
+
