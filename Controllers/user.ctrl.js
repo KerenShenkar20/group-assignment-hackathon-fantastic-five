c1d740a2840dc26e54dda7e4f3d75ed7b06f6d88
saarp <saarp@liveperson.com>
2020-12-18 11:33:06 +0200

    user.ctrl first update

--- Controllers/user.ctrl.js
+++ Controllers/user.ctrl.js
@@ -1,1 +1,54 @@
-// Change the name of the file if needed
+
+const User = require('../Models/user');
+
+exports.userController = {
+    getUsers(req, res) {
+        User.find({})
+            .then(docs => { res.json(docs) })
+            .catch(err => console.log(`Error getting the data from DB: ${err}`));
+    },
+    getUser(req, res) {
+        User.findOne({ id: req.params.id })
+            .then(docs => { res.json(docs) })
+            .catch(err => console.log(`Error getting the data from DB: ${err}`));
+    },
+
+    filterUsers(req, res) {
+        let query = {}
+        if (req.query.job){
+            query.job = req.query.job
+        }
+        if (req.query.gender){
+            query.gender = req.query.gender
+        }
+        if (req.query.email){
+            query.email = req.query.email
+        }
+        console.log(query);
+        User.find(query)
+            .then(docs => { res.json(docs) })
+            .catch(err => console.log(`Error getting the data from DB: ${err}`));
+    },
+
+    addUser(req, res) {
+        const { body } = req;
+        const newUser = new User(body);
+        const result = newUser.save();
+        if (result) {
+            res.json(newUser)
+        } else {
+            res.status(404).send("Error saving a restaurant");
+        }
+    },
+    updateUser(req, res) {
+        const { body } = req;
+        User.updateOne({ id: req.params.id }, body)
+            .then(docs => { res.json(docs) })
+            .catch(err => console.log(`Error updating restaurant from db: ${err}`));
+    },
+    deleteUser(req, res) {
+        User.deleteOne({ id: req.params.id })
+            .then(docs => { res.json(docs) })
+            .catch(err => console.log(`Error deleting user from db: ${err}`));
+    }
+};
